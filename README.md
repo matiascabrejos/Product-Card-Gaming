f004a130ee7bdb53503119bd329bdc3676546482
Matias Cabrejos <matias.cabrejos@hotmail.com>
2021-03-26 17:09:46 -0300

    Serving static public folder

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ $ npm init -y
 # Installs Express
 $ npm i express
 ```
+
+## [Express docs](http://expressjs.com/en/starter/hello-world.html)
+
+## [Serve static files](http://expressjs.com/en/starter/static-files.html#serving-static-files-in-express)
--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('index.html', (req, res) => res.send('Hello World!'))
+
+app.use(express.static('public'))
 app.listen(port, () => console.log(`Example app listening on port port!`))
